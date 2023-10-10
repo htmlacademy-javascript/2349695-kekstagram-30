f02da4496ab7e02642b650e399a862ee4ae2e66d
Анастасия <an9749594@gmail.com>
2023-10-11 00:21:32 +0300

    Добавляет доп.задание после просмотра ретро

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,13 @@ const checkLength = function (string, maxLength) {
   return string.length <= maxLength;
 };
 
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 10);
+
 //Функция для проверки, является ли строка палиндромом
 
-const checkPalindrome = function (line) {
-  const normalizeLine = line.toLowerCase().replaceAll(/\s/g,'');
+const checkPalindrome = function (string) {
+  const normalizeLine = string.toLowerCase().replaceAll(/\s/g,'');
   let emptyLine = '';
   for (let i = normalizeLine.length - 1; i >= 0; i--) {
     emptyLine += normalizeLine.at(i);
@@ -15,6 +18,20 @@ const checkPalindrome = function (line) {
 
 };
 
+checkPalindrome('топот');
+checkPalindrome('Кекс');
+
 //Дополнительное задание
 
+const returnNumber = function (inputData) {
+  const string = inputData.toString();
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
+    }
+  }
+  return parseInt(result, 10);
+};
 
+returnNumber('1 кефир, 0.5 батона');
