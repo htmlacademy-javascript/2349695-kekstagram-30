756f793c299876082245feb106215085094b84f0
Анастасия <an9749594@gmail.com>
2023-10-10 22:25:36 +0300

    Добавляет вторую функцию

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,20 @@
-//Первая функция
+//Функция для проверки длины строки
 const checkLength = function (string, maxLength) {
   return string.length <= maxLength;
 };
 
-//Вторая функция
+//Функция для проверки, является ли строка палиндромом
+
+const checkPalindrome = function (line) {
+  const normalizeLine = line.toLowerCase().replaceAll(/\s/g,'');
+  let emptyLine = '';
+  for (let i = normalizeLine.length - 1; i >= 0; i--) {
+    emptyLine += normalizeLine.at(i);
+  }
+  return emptyLine === normalizeLine;
+
+};
+
+//Дополнительное задание
+
+
