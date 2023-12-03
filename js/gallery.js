063f746967be13224db248914de14a7a5be1ad6b
Анастасия <an9749594@gmail.com>
2023-12-03 14:01:35 +0300

    Правки после проверки

--- js/gallery.js
+++ js/gallery.js
@@ -2,22 +2,24 @@ import { renderPictureElements } from './rendering.js';
 import { showPicture } from './full-picture.js';
 
 const picturesPlace = document.querySelector('.pictures');
+let showedPictures = [];
+
+picturesPlace.addEventListener('click', (evt) => {
+  const pictureElement = evt.target.closest('[data-picture-id]');
+
+  if (!pictureElement) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  const pictureId = +pictureElement.dataset.pictureId;
+  const pictureData = showedPictures.find(({ id }) => id === pictureId);
+  showPicture(pictureData);
+});
 
 const renderGallery = (pictures) => {
-  picturesPlace.addEventListener('click', (evt) => {
-    const pictureElement = evt.target.closest('[data-picture-id]');
-
-    if (!pictureElement) {
-      return;
-    }
-
-    evt.preventDefault();
-
-    const pictureId = +pictureElement.dataset.pictureId;
-    const pictureData = pictures.find(({ id }) => id === pictureId);
-    showPicture(pictureData);
-
-  });
+  showedPictures = pictures;
   renderPictureElements(pictures, picturesPlace);
 };
 
